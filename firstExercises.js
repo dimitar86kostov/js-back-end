eda8578bb38010fd8986e300dd39d5135d8ce242
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2024-05-13 00:42:49 +0300

    just start

---
+++ firstExercises.js
@@ -0,0 +1,15 @@
+class Az {
+    constructor(data, fun, gun) {
+        this.data = data;
+        this.fun = fun;
+        this.gun = gun;
+    }
+
+    get data() {
+        return this.data
+    }
+
+    set data(great) {
+        this.data = great;
+    }
+}
