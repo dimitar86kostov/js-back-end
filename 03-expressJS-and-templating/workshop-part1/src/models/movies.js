f0aa3c2d941ed5ba127446948893b421de7bfd0e
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2024-05-31 00:23:04 +0300

    add new functionality for DB

--- 03-expressJS-and-templating/workshop-part1/src/models/movies.js
+++ 03-expressJS-and-templating/workshop-part1/src/models/movies.js
@@ -1,13 +1,48 @@
-class Movie {
-    id;
-    title;
-    genre;
-    director;
-    year;
-    imageURL;
-    description;
-}
+const { Schema, SchemaType: Types, model } = require('mongoose')
 
-module.exports = {
-    Movie
-}
+const movieSchema = new Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    director: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true,
+        min: 1878,
+        max: 2100
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 5
+    },
+    imageURL: {
+        type: String,
+        required: true,
+        RegExp: /^https?:\/\/.+/
+    },
+    description: {
+        type: String,
+        maxLength: 1000
+    },
+    cast: {
+        type: [Types.ObjectId],
+        ref: 'Cast',
+        default: []
+    }
+})
+
+const Movie = model('movies', movieSchema);
+
+module.exports = { Movie }
+
--- 03-expressJS-and-templating/workshop-part1/src/services/movie.js
+++ 03-expressJS-and-templating/workshop-part1/src/services/movie.js
@@ -1,65 +1,30 @@
-const fs = require('fs/promises');
 const { Movie } = require('../models/movies');
 
-const filePath = './data/database.json';
-
-async function readFile() {
-    const data = await fs.readFile(filePath);
-
-    return JSON.parse(data.toString());
-};
-
-async function writeFile(data) {
-    await fs.writeFile(filePath, JSON.stringify(data));
-};
-
-function toMovieModel(data) {
-    const movie = new Movie();
-
-    movie.id = data.id;
-    movie.title = data.title;
-    movie.genre = data.genre;
-    movie.director = data.director;
-    movie.year = data.year;
-    movie.imageURL = data.imageURL;
-    movie.rating = data.rating;
-    movie.description = data.description;
-
-    return movie;
-}
-
 async function getAllMovies() {
-    const movies = await readFile();
-    return movies.map(toMovieModel);
+    return await Movie.find().lean();
 }
 
 async function getMovieById(id) {
-    const movies = await readFile();
-    let movie = movies.find(m => m.id == id);
 
-    return movie ? toMovieModel(movie) : movie;
+    return await Movie.findById(id).lean();
+
 }
 
 async function createMovie(data) {
 
-    const id = uuid();
-
-    const movie = {
-        id,
+    const movie = new Movie({
         title: data.title,
         genre: data.genre,
         director: data.director,
         year: Number(data.year),
-        imageURL: data.imageURL,
         rating: Number(data.rating),
-        description: data.description
-    }
+        description: data.description,
+        imageURL: data.imageURL,
+        cast: []
+    });
 
-    const movies = await readFile();
-    movies.push(movie);
-    await writeFile(movies);
-
-    return toMovieModel(movie);
+    await movie.save();
+    return movie;
 }
 
 function uuid() {
