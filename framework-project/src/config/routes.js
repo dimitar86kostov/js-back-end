f6ce789a0ff9d078103cf80f6c82616dbef2e12c
dimitar86kostov <145497494+dimitar86kostov@users.noreply.github.com>
2024-06-06 15:44:00 +0300

    add starting main template with home controller

--- framework-project/src/config/routes.js
+++ framework-project/src/config/routes.js
@@ -1,5 +1,8 @@
 const { Router } = require('express');
+const { homeController } = require('../controllers/catalog');
 
 const router = Router();
+
+router.get('/', homeController);
 
 module.exports = { router };
